81a1d63a87868fbeb989b43a514906f9d6dd3e06
Vintage Vivek <viveksir58@gmail.com>
2024-07-14 19:27:06 +0530

    Add operators

--- Day1/Operators.java
+++ Day1/Operators.java
@@ -3,11 +3,41 @@ package Day1;
 public class Operators {
    public static void main(String[] args) {
     
-        int a = 5;
-        int b = 6;
+        int a = 55;
+        int b = 5;
         int sum = a + b;
         System.out.println(sum);
+        System.out.println("Added all operators");
 
+
+         // PRIMITIVE
+         // int         = integer ----------------------- 4 bytes
+         // float       = decimal upto 6 decimals ------- 4 bytes 
+         // double      = decimal upto 15 decimal ------- 8 bytes
+         // char        = character --------------------- 2 bytes 
+         // long        = integer ----------------------- 8 bytes 
+         // boolean     = true or false ----------------- 1 bit 
+         // short       = number ------------------------- 2 bytes
+
+         // 1. ARITHMATIC OPERATORS : +, -, /, %, ++, --
+
+         // 2. COMPARISON OPERATORS : ==,!=,>,<,>=,<=
+         
+         // 3. LOGICAL OPERATORS : &&,||,!
+
+         // HOMEWORK : 
+         // 4. ASSIGNMENT OPERATORS : =, +=, -=, *=, /=, %=, |= 
+         // int x=1; x +=3;  x=x+3 
+         //          x -=3;  x=x-3 
+         //          x *=3;  x=x*3 
+         //          x /=3;  x=x/3 
+         //          x %=3;  x=x%3
+
+
+         // HOMEWORK : 
+         // 5. BITWISE OPERATORS - & , | , ~ , << , >> , >>>
+        
+        
 
    }
     
