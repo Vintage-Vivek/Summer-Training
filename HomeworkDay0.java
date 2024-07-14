0057013e0c59c86e0ca5f88ce991000892630544
Vintage Vivek <viveksir58@gmail.com>
2024-07-14 13:27:37 +0530

    First merge

--- HomeworkDay0.java
+++ HomeworkDay0.java
@@ -1,5 +1,6 @@
 public class HomeworkDay0 {
     public static void main(String[] args) {
         System.out.println("Homework for pushing");
+        System.out.println("Here are edit from the new branch");
     }
 }
