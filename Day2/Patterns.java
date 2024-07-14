61d1330e818d4b54d23183ca580eb928b5b384f2
Vintage Vivek <viveksir58@gmail.com>
2024-07-14 23:15:09 +0530

    Pattern files

---
+++ Day2/Patterns.java
@@ -0,0 +1,31 @@
+package Day2;
+import java.util.Scanner;
+import java.util.*;
+
+public class Patterns {
+    public static void main(String[] args) {
+        System.out.println(" ");
+        int n= 5;
+        
+
+        // * * * * *  
+        // * * * * *  
+        // * * * * *  
+        // * * * * *  
+        // * * * * * 
+
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                 System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+
+        System.out.println(" ");
+
+    }
+
+
+    
+}
