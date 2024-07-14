8fc2670bad04a2f07949d094f049a973902fdb8a
Vintage Vivek <viveksir58@gmail.com>
2024-07-14 22:36:21 +0530

    Add loops

---
+++ Day2/Loops.java
@@ -0,0 +1,49 @@
+package Day2;
+
+public class Loops {
+public static void main(String[] args) {
+    
+        int n= 5;
+        System.out.println(" ");
+    
+       for(int i=1; i<=10; i++){
+        System.out.print(i +" ");
+       }
+    
+       System.out.println(" -------------------1st Loop ");
+       System.out.println(" ");
+
+       for(int i=20; i<=30; i++){
+        System.out.print(i +" ");
+       }
+
+       System.out.println(" -------2nd Loop");
+       System.out.println(" ");
+
+       
+       for(int i=0; i<=n; i++){
+        System.out.print(2*i+1 + " ");
+       }
+
+       System.out.println(" ---------------------------3rd Loop");
+       System.out.println(" ");
+
+
+       for(int i=10; i>0; i--){
+        System.out.print(i + " ");
+       }
+
+       System.out.println(" -------------------4th Loop");
+       System.out.println(" ");
+    
+
+       for(int i=n; i>0; i--){
+        System.out.print(i + " ");
+       }
+
+       System.out.println(" ------------------------------5th Loop");
+       System.out.println(" ");
+
+    }
+    
+}
