a986ba50fe4baf9dcaf9513e8b2955ba8eac09fa
Vintage Vivek <viveksir58@gmail.com>
2024-07-14 17:25:45 +0530

    Add files to Day0

---
+++ Day0/HomeworkDay0.java
@@ -0,0 +1,7 @@
+public class HomeworkDay0 {
+    public static void main(String[] args) {
+        System.out.println("Homework for pushing");
+        System.out.println("Here are edit from the new branch");
+        System.out.println("testing");
+    }
+}
