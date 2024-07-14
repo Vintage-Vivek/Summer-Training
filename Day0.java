31ff245503021e89f090703bc11d0227f994f7a6
Vintage Vivek <viveksir58@gmail.com>
2024-07-14 18:56:59 +0530

    unnecessary

--- Day0.java
+++
@@ -1,5 +0,0 @@
-public class Day0{
-    public static void main(String[] args) {
-        System.out.println("Hello, here we go again");
-    }
-}
--- HomeworkDay0.java
+++
@@ -1,7 +0,0 @@
-public class HomeworkDay0 {
-    public static void main(String[] args) {
-        System.out.println("Homework for pushing");
-        System.out.println("Here are edit from the new branch");
-        System.out.println("testing");
-    }
-}
