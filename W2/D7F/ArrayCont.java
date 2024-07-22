dc738edeff65a600fcabbd5fdad790bef520d01b
Vintage Vivek <viveksir58@gmail.com>
2024-07-22 14:11:08 +0530

    dutch algo

---
+++ W2/D7F/ArrayCont.java
@@ -0,0 +1,7 @@
+public class ArrayCont {
+    public static void main(String[] args) {
+        long l= Long.MAX_VALUE;
+        System.out.println(l);
+        System.out.println("Leet code solutions solved");
+    }
+}
