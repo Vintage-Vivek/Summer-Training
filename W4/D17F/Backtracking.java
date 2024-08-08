b461e68afdb4f55c5db8eabbe9f5a68a300c8dcf
Vintage Vivek <viveksir58@gmail.com>
2024-08-08 15:05:38 +0530

    RAIM

---
+++ W4/D17F/Backtracking.java
@@ -0,0 +1,8 @@
+package W4.D17F;
+
+public class Backtracking {
+    public static void main(String[] args) {
+        System.out.println("Two questions of leetcode question no. 39 and 46 done for backtracking in this class");
+    }
+    
+}
