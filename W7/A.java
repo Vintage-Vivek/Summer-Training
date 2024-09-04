74aa37600308ec72052f2f822d7908db3a27609b
Vintage Vivek <viveksir58@gmail.com>
2024-09-04 16:21:46 +0530

    oops again

---
+++ W7/A.java
@@ -0,0 +1,10 @@
+package W7;
+
+public class A {
+    int a;
+    A(int a){ //int val
+        //a=val;
+        //a=a;
+        this.a=a;// local variable and instance variable change this
+    } 
+}
