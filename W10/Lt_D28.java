2cd9bd590c4e76cde88a2f7486a3d3c0a8dc0562
Vintage Vivek <viveksir58@gmail.com>
2024-11-03 15:34:43 +0530

    lt

--- W10/Lt_D28.java
+++ W10/Lt_D28.java
@@ -1,6 +1,9 @@
 package W10;
 
 public class Lt_D28 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
     //876-tortoise and heir problem
     //141
     //206
